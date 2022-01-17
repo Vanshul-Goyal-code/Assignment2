51b43756dd3b51f60cd4a040570edd7cd91fe63e
Vanshul-Goyal-code <96768035+Vanshul-Goyal-code@users.noreply.github.com>
2022-01-17 17:24:18 +0530

    Add files via upload

---
+++ Output2.py
@@ -0,0 +1,71 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment2.py/Q1.py ===========
+Question1
+
+Part a 
+Length of Input String = 35
+
+Part b
+egaugnal evitisnes esac a si nohtyP
+
+Part c
+a case sensitive
+
+Part d
+Python is object oriented language
+
+Part e
+10
+
+Part f
+Pythonisacasesensitivelanguage
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment2.py/Q2.py ===========
+Question 2
+
+Hey, Vanshul Goyal here! 
+My SID is 21105079 
+I am from ECE and my CGPA is 10
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment2.py/Q3.py ===========
+Question 3
+ a&b :  8
+ a|b :  58
+ a^b :  50
+a<<2 :  224 	b<<2 : 40
+a>>2 :  14 	b>>2 : 2
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment2.py/Q4.py ===========
+Question 4
+Enter 1st no. : 5
+Enter 2nd no. : 6
+Enter 3rd no. : 7
+Highest no. = 7
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment2.py/Q5.py ===========
+Qustion 5
+Enter input string: hello name
+Yes
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment2.py/Q5.py ===========
+Qustion 5
+Enter input string: hello
+No
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment2.py/Q6.py ===========
+Question 6
+
+Enter 1st length : 12
+Enter 2nd length : 13
+Enter 3rd length : 5
+Yes
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment2.py/Q6.py ===========
+Question 6
+
+Enter 1st length : 12
+Enter 2nd length : 5
+Enter 3rd length : 1
+No
